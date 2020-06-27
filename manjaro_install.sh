3069167770e3c69f29933ca55e2e6a8333df18f8
kausik6das3 <32133686+kausik6das3@users.noreply.github.com>
2020-06-27 21:38:44 +0530

    Add files via upload

---
+++ manjaro_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo pacman -Syu <<-a
+y
+a
+sudo pacman -S --noconfirm --needed gcc gcc-fortran cmake pkg-config
+sudo pacman -S --noconfirm --needed gnuplot emacs vim
+sudo pacman -S --noconfirm --needed hugo texatudio
+sudo pacman -S --noconfirm --needed pandoc pandoc-citeproc
+sudo pacman -S --noconfirm --needed gimp
+sudo pacman -S --noconfirm --needed git dkms
+sudo pacman -S --noconfirm --needed redshift 
+#_________For_mesa_OpenGL_________________________#
+sudo pacman -S --noconfirm --needed libgl mesa 
+#_________For_Dwm_________________________________#
+sudo pacman -S --noconfirm --needed dmenu 
+sudo pacman -S --noconfirm --needed compton
+sudo pacman -S --noconfirm --needed feh
+sudo pacman -S --noconfirm --needed xorg-xsetroot xorg-xbacklight 
+sudo pacman -S --noconfirm --needed playerctl compton 
+sudo pacman -S --noconfirm --needed feh i3lock
