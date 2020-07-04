4552aa743d9011d4485d187902f8e4b0525c0cb5
kausik6das3 <32133686+kausik6das3@users.noreply.github.com>
2020-07-04 22:43:34 +0530

    Add files via upload

---
+++ i3lock.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#~/.config/dwm
+revert() {
+xset dpms 0 0 0
+}
+trap revert HUP INT TERM
+xset +dpms dpms 5 5 5
+i3lock  -e -i /home/kausik/.local/share/lc.png -n
+revert
+
--- manjaro_install.sh
+++ manjaro_install.sh
@@ -4,11 +4,12 @@ y
 a
 sudo pacman -S --noconfirm --needed gcc gcc-fortran cmake pkg-config
 sudo pacman -S --noconfirm --needed gnuplot emacs vim
-sudo pacman -S --noconfirm --needed hugo texatudio
+sudo pacman -S --noconfirm --needed hugo texatudio 
 sudo pacman -S --noconfirm --needed pandoc pandoc-citeproc
-sudo pacman -S --noconfirm --needed gimp
+sudo pacman -S --noconfirm --needed gimp imagemagick
 sudo pacman -S --noconfirm --needed git dkms
 sudo pacman -S --noconfirm --needed redshift 
+sudo pacman -S --noconfirm --needed liferea
 #_________For_mesa_OpenGL_________________________#
 sudo pacman -S --noconfirm --needed libgl mesa 
 #_________For_Dwm_________________________________#
@@ -17,4 +18,4 @@ sudo pacman -S --noconfirm --needed compton
 sudo pacman -S --noconfirm --needed feh
 sudo pacman -S --noconfirm --needed xorg-xsetroot xorg-xbacklight 
 sudo pacman -S --noconfirm --needed playerctl compton 
-sudo pacman -S --noconfirm --needed feh i3lock
+sudo pacman -S --noconfirm --needed i3lock
