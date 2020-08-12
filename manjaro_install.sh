a797e99612143310f3d7567861f0c9c6b9958459
Kausik6das3 <kausik6das3@gmail.com>
2020-08-12 23:19:14 +0530

    Internal Commit

--- manjaro_install.sh
+++ manjaro_install.sh
@@ -6,17 +6,17 @@ sudo pacman -S --noconfirm --needed gcc gcc-fortran cmake pkg-config
 sudo pacman -S --noconfirm --needed gnuplot emacs vim
 sudo pacman -S --noconfirm --needed hugo texatudio 
 sudo pacman -S --noconfirm --needed pandoc pandoc-citeproc
-sudo pacman -S --noconfirm --needed gimp imagemagick
+sudo pacman -S --noconfirm --needed gimp imagemagick inkscape
 sudo pacman -S --noconfirm --needed git dkms
 sudo pacman -S --noconfirm --needed redshift powertop 
-sudo pacman -S --noconfirm --needed liferea
+sudo pacman -S --noconfirm --needed liferea 
 sudo pacman -S --noconfirm --needed rhythmbox cmus
+#_________For_mesa_OpenGL_________________________#
+sudo pacman -S --noconfirm --needed libgl mesa 
 #_________For_OpenCL_ob_Gen7_Ivy Bridge_Intel_GPU_____#
 sudo pacman -S --noconfirm --needed beignet ocl-icd opencl-headers
 #_________For_Emoji_______________________________#
 #sudo pacman -S --noconfirm --needed noto-fonts-emoji ttf-joypixels 
-#_________For_mesa_OpenGL_________________________#
-sudo pacman -S --noconfirm --needed libgl mesa 
 #_________For_Dwm_________________________________#
 sudo pacman -S --noconfirm --needed dmenu 
 sudo pacman -S --noconfirm --needed picom
